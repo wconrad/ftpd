b146b3d98de411856d4ec3c9e45db707dc19d001
Wayne Conrad <wayne@databill.com>
2013-02-20 08:16:22 -0700

    Removed dead code from FtpServer

--- lib/ftpd/ftp_server.rb
+++ lib/ftpd/ftp_server.rb
@@ -6,14 +6,10 @@ module Ftpd
     attr_accessor :debug_path
     attr_accessor :driver
     attr_accessor :implicit_tls
-    attr_accessor :password
     attr_accessor :response_delay
-    attr_accessor :user
 
     def initialize(opts = {})
       super
-      self.user = 'user'
-      self.password = 'password'
       self.debug_path = '/dev/stdout'
       @driver = MissingDriver.new
       @response_delay = 0
@@ -23,8 +19,6 @@ module Ftpd
     def session(socket)
       Session.new(:socket => socket,
                   :driver => @driver,
-                  :user => user,
-                  :password => password,
                   :debug_path => debug_path,
                   :response_delay => response_delay,
                   :implicit_tls => @implicit_tls).run
