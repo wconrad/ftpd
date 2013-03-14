496176ac74ebb6b115a79083812bcbd721a13bc1
Wayne Conrad <wayne@databill.com>
2013-03-14 07:53:29 -0700

    FtpServer attributes in alphabetical order

--- lib/ftpd/ftp_server.rb
+++ lib/ftpd/ftp_server.rb
@@ -8,60 +8,6 @@ module Ftpd
     DEFAULT_SERVER_NAME = 'wconrad/ftpd'
     DEFAULT_SESSION_TIMEOUT = 300 # seconds
 
-    # The number of seconds to delay before replying.  This is for
-    # testing, when you need to test, for example, client timeouts.
-    # Defaults to 0 (no delay).
-    #
-    # Set this before calling #start.
-
-    attr_accessor :response_delay
-
-    # The class for formatting for LIST output.  Defaults to
-    # {Ftpd::ListFormat::Ls}.
-    #
-    # Set this before calling #start.
-
-    attr_accessor :list_formatter
-
-    # @return [Integer] The authentication level
-    # One of:
-    # * Ftpd::AUTH_USER
-    # * Ftpd::AUTH_PASSWORD (default)
-    # * Ftpd::AUTH_ACCOUNT
-
-    attr_accessor :auth_level
-
-    # The session timeout.  When a session is awaiting a command, if
-    # one is not received in this many seconds, the session is
-    # disconnected.  Defaults to {DEFAULT_SESSION_TIMEOUT}.  If nil,
-    # then timeout is disabled.
-    # @return [Numeric]
-    #
-    # Set this before calling #start.
-
-    attr_accessor :session_timeout
-
-    # The server's name, sent in a STAT reply.  Defaults to
-    # {DEFAULT_SERVER_NAME}.
-    #
-    # Set this before calling #start.
-
-    attr_accessor :server_name
-
-    # The server's version, sent in a STAT reply.  Defaults to the
-    # contents of the VERSION file.
-    #
-    # Set this before calling #start.
-
-    attr_accessor :server_version
-
-    # The logger.  Defaults to nil (no logging).
-    # @return [Logger]
-    #
-    # Set this before calling #start.
-
-    attr_accessor :log
-
     # Allow PORT command to specify data ports below 1024.  Defaults
     # to false.  Setting this to true makes it easier for an attacker
     # to use the server to attack another server.  See RFC 2577
@@ -71,6 +17,28 @@ module Ftpd
     # Set this before calling #start.
 
     attr_accessor :allow_low_data_ports
+
+    # @return [Integer] The authentication level
+    # One of:
+    # * Ftpd::AUTH_USER
+    # * Ftpd::AUTH_PASSWORD (default)
+    # * Ftpd::AUTH_ACCOUNT
+
+    attr_accessor :auth_level
+
+    # The class for formatting for LIST output.  Defaults to
+    # {Ftpd::ListFormat::Ls}.
+    #
+    # Set this before calling #start.
+
+    attr_accessor :list_formatter
+
+    # The logger.  Defaults to nil (no logging).
+    # @return [Logger]
+    #
+    # Set this before calling #start.
+
+    attr_accessor :log
 
     # The maximum number of connections the server will allow
     # Defaults to {ConnectionThrottle::DEFAULT_MAX_CONNECTIONS}.
@@ -94,6 +62,38 @@ module Ftpd
 
     def_delegator :@connection_throttle, :'max_connections_per_ip'
     def_delegator :@connection_throttle, :'max_connections_per_ip='
+
+    # The number of seconds to delay before replying.  This is for
+    # testing, when you need to test, for example, client timeouts.
+    # Defaults to 0 (no delay).
+    #
+    # Set this before calling #start.
+
+    attr_accessor :response_delay
+
+    # The server's name, sent in a STAT reply.  Defaults to
+    # {DEFAULT_SERVER_NAME}.
+    #
+    # Set this before calling #start.
+
+    attr_accessor :server_name
+
+    # The server's version, sent in a STAT reply.  Defaults to the
+    # contents of the VERSION file.
+    #
+    # Set this before calling #start.
+
+    attr_accessor :server_version
+
+    # The session timeout.  When a session is awaiting a command, if
+    # one is not received in this many seconds, the session is
+    # disconnected.  Defaults to {DEFAULT_SESSION_TIMEOUT}.  If nil,
+    # then timeout is disabled.
+    # @return [Numeric]
+    #
+    # Set this before calling #start.
+
+    attr_accessor :session_timeout
 
     # Create a new FTP server.  The server won't start until the
     # #start method is called.
