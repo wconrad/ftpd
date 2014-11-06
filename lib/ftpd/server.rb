50737fedb667d9e0d13e95a93730a358b0b24f53
André Aizim Kelmanson <akelmanson@gmail.com>
2014-11-06 16:42:48 -0200

    adding Server#join

--- lib/ftpd/server.rb
+++ lib/ftpd/server.rb
@@ -37,6 +37,13 @@ module Ftpd
       @server_socket.addr[1]
     end
 
+    # The calling thread will suspend execution until the server is
+    # stopped.
+
+    def join
+      @server_thread.join
+    end
+
     # Start the server.  This creates the server socket, and the
     # thread to service it.
 
