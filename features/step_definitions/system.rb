929d9263c541098a57ac32d4f1ab3dc2d403ded4
Wayne Conrad <kf7qga@gmail.com>
2016-01-25 14:10:36 -0700

    Test for "system" command passes in Ruby 2.3

--- features/step_definitions/system.rb
+++ features/step_definitions/system.rb
@@ -1,5 +1,8 @@
 When /^the client successfully queries system ID$/ do
   @reply = client.system
+  # Prior to ruby-2.3.0, the #system call returned a string ending in
+  # a new-line.
+  @reply += "\n" unless @reply =~ /\n$/
 end
 
 Then /^the server returns a system ID reply$/ do
